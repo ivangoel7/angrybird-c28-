89b8c9af44fc5c2a4110cb44e65616ebdb3b1bd3
ivangoel <76724871+ivangoel7@users.noreply.github.com>
2021-04-08 23:20:59 +0530

    Update sketch.js
    hahahahaha bird go vrooooooooooooooooooooooom      red go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+//ivanop
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
